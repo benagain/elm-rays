c7cefc3bc55e62aaa0bfca2bdf50a2fade7f55b3
Ben Arnold <ben@arnoldfamily.me.uk>
2016-09-27 14:32:06 +0100

    Open URLs in the mobile browser

--- www/js/index.js
+++ www/js/index.js
@@ -3,3 +3,8 @@ var elm_app = Elm.Main.embed(document.getElementById('elm-main'));
 
 // Cordova initialisation if needed
 // ...
+$(document).on('click', 'a', function (event) {
+    event.preventDefault();
+    window.open($(this).attr('href'), '_system');
+    return false;
+});
